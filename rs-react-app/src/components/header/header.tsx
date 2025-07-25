28a5b6aa58d4d374c8571de1f2fbb0f9864db5ce
Xomkalol <shakovxomka@gmail.com>
2025-07-26 01:00:35 +0500

    feat: header class to function migrate

--- rs-react-app/src/components/header/header.tsx
+++ rs-react-app/src/components/header/header.tsx
@@ -1,55 +1,49 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import './header.css';
 
-type HeaderState = {
-  searchValue: string;
-};
 interface HeaderProps {
   onSearch: (query: string) => void;
 }
 
-class Header extends Component<HeaderProps, HeaderState> {
-  state: HeaderState = {
-    searchValue: '',
-  };
-  componentDidMount(): void {
+export default function Header({ onSearch }: HeaderProps) {
+  const [searchValue, setSearchValue] = useState('');
+
+  useEffect(() => {
     const savedValue = localStorage.getItem('searchValue');
     if (savedValue) {
-      this.setState({ searchValue: savedValue }, () => {
-        this.searchPokemon();
-      });
+      setSearchValue(savedValue);
+      onSearch(savedValue);
+      localStorage.setItem('searchValue', savedValue);
     }
-  }
-  render() {
-    return (
-      <div className="upper__container" data-testid="upper-container">
-        <div className="upper__controls-wrapper">
-          <div className="controls__input-wrapper">
-            <input
-              id="search-input"
-              value={this.state.searchValue}
-              onChange={(e) => this.setState({ searchValue: e.target.value })}
-              onKeyUp={(e) => e.key === 'Enter' && this.searchPokemon()}
-              data-testid="search-input"
-            />
-          </div>
-          <div className="controls__button-wrapper">
-            <button
-              className="controls__button"
-              onClick={this.searchPokemon}
-              data-testid="search-button"
-            >
-              Search
-            </button>
-          </div>
+  }, [onSearch]);
+
+  const handleSearch = () => {
+    onSearch(searchValue);
+    localStorage.setItem('searchValue', searchValue);
+  };
+
+  return (
+    <div className="upper__container" data-testid="upper-container">
+      <div className="upper__controls-wrapper">
+        <div className="controls__input-wrapper">
+          <input
+            id="search-input"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            onKeyUp={(e) => e.key === 'Enter' && handleSearch()}
+            data-testid="search-input"
+          />
+        </div>
+        <div className="controls__button-wrapper">
+          <button
+            className="controls__button"
+            onClick={() => handleSearch()}
+            data-testid="search-button"
+          >
+            Search
+          </button>
         </div>
       </div>
-    );
-  }
-  searchPokemon = () => {
-    this.props.onSearch(this.state.searchValue);
-    localStorage.setItem('searchValue', this.state.searchValue);
-  };
+    </div>
+  );
 }
-
-export default Header;
