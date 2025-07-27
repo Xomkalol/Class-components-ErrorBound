ace13db999a493ed0b095c153d4a40f8ed4d2e94
Xomkalol <shakovxomka@gmail.com>
2025-07-27 11:05:48 +0500

    feat: first react router data implementation

--- rs-react-app/src/main.tsx
+++ rs-react-app/src/main.tsx
@@ -2,8 +2,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './colors.css';
+import { createBrowserRouter, RouterProvider } from 'react-router';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
