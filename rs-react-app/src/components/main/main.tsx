8b139429a452d0e2f4e08548735ebb57b6ba8340
Xomkalol <shakovxomka@gmail.com>
2025-08-03 16:08:27 +0500

    feat: main dark theme

--- rs-react-app/src/components/main/main.tsx
+++ rs-react-app/src/components/main/main.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import './main.css';
 import Skeleton from '../skeleton/skeleton';
 import ErrorBoundary from '../errorBoundary/errorBoundary';
@@ -7,6 +7,7 @@ import { Outlet, useNavigate, useParams, useSearchParams } from 'react-router';
 import { apiLink } from '../api/apiHandler';
 import Checkbox from './checkbox';
 import FlyOut from '../flyout/flyout';
+import { themeContext } from '../../util/context';
 
 interface MainProps {
   pokemons: { name: string; url: string }[];
@@ -31,6 +32,8 @@ export default function Main({
   const [searchParams] = useSearchParams();
   const [selectedPokemonUrl, setselectedPokemonUrl] = useState('');
   const [showSkeleton, setshowSkeleton] = useState(true);
+  const { theme } = useContext(themeContext);
+
   const navigate = useNavigate();
   useEffect(() => {
     setTimeout(() => {
@@ -66,44 +69,49 @@ export default function Main({
   };
   return (
     <ErrorBoundary>
-      <main className="main__container">
-        <h2 className="main__header">Pokemons</h2>
+      <main className={`main__container ${theme}`}>
+        <h2 className={`main__header ${theme}`}>Pokemons</h2>
 
         {error && <ApiErrorBanner error={error} onRetry={onRetry} />}
-        <FlyOut></FlyOut>
-        <div className="result__wrapper">
-          <div className="result__header">
-            <span className="header__text">Pokemon Name</span>
-            <span className="header__text">Details</span>
+        <FlyOut />
+
+        <div className={`result__wrapper ${theme}`}>
+          <div className={`result__header ${theme}`}>
+            <span className={`header__text ${theme}`}>Pokemon Name</span>
+            <span className={`header__text ${theme}`}>Details</span>
           </div>
-          <div className="results__main">
+
+          <div className={`results__main ${theme}`}>
             {isLoading || showSkeleton ? (
               <Skeleton count={8} />
             ) : pokemons.length > 0 ? (
               pokemons.map((pokemon) => (
-                <div key={pokemon.name} className="main__item">
-                  <div className="item__name-wrapper">
-                    <span className="item__name">{pokemon.name}</span>
+                <div key={pokemon.name} className={`main__item ${theme}`}>
+                  <div className={`item__name-wrapper ${theme}`}>
+                    <span className={`item__name ${theme}`}>
+                      {pokemon.name}
+                    </span>
                   </div>
-                  <div className="item__description-wrapper">
+                  <div className={`item__description-wrapper ${theme}`}>
                     <span
-                      className="item__description"
+                      className={`item__description ${theme}`}
                       onClick={() => handleShowPokemon(pokemon.url)}
                     >
                       View details
                     </span>
-                    <Checkbox pokemonUrl={pokemon.url}></Checkbox>
+                    <Checkbox pokemonUrl={pokemon.url} />
                   </div>
                 </div>
               ))
             ) : (
-              <div className="no-results">
+              <div className={`no-results ${theme}`}>
                 No pokemons found. Try a different search.
               </div>
             )}
           </div>
         </div>
-        <div className="button-wrapper">
+
+        <div className={`button-wrapper ${theme}`}>
           <button
             onClick={() => handlePrevPaginationButton()}
             disabled={currentOffset === 0 || isLoading}
