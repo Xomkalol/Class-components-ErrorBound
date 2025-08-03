79973bebdd7a3fa2543a3e1e0314a0b0d180dbd0
Xomkalol <shakovxomka@gmail.com>
2025-08-03 16:49:01 +0500

    feat: flyout styles

--- rs-react-app/src/components/flyout/flyout.tsx
+++ rs-react-app/src/components/flyout/flyout.tsx
@@ -2,26 +2,36 @@ import { useDispatch, useSelector } from 'react-redux';
 import type { RootState } from '../../store/store';
 import { clearState } from '../../store/counter';
 import CreateCSVFile from '../../util/createCSV';
+import './flyout.css';
+import { themeContext } from '../../util/context';
+import { useContext } from 'react';
 
 export default function FlyOut() {
   const pokeMonState = useSelector((state: RootState) => state.counter);
   const dispatch = useDispatch();
+  const { theme } = useContext(themeContext);
 
   if (pokeMonState.urls.length == 0) {
     return null;
   }
 
   return (
-    <div className="main-selected">
-      {pokeMonState.urls.map((item) =>
-        pokeMonState.urls.length > 0 ? (
-          <div key="item">{item}</div>
-        ) : (
-          <div key="nothing"></div>
-        )
-      )}
-      <button onClick={() => dispatch(clearState())}>Unselect</button>
-      <button onClick={() => CreateCSVFile(pokeMonState)}>Download</button>
+    <div className={`main-selected ${theme}`}>
+      {pokeMonState.urls.map((item) => (
+        <div key={item}>{item}</div>
+      ))}
+      <button
+        className={`selected__unselect-button ${theme}`}
+        onClick={() => dispatch(clearState())}
+      >
+        Unselect
+      </button>
+      <button
+        className={`selected__download-button ${theme}`}
+        onClick={() => CreateCSVFile(pokeMonState)}
+      >
+        Download
+      </button>
     </div>
   );
 }
