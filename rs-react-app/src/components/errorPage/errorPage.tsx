4788cab5bd27dfd89fc13989242df63d072926c8
Xomkalol <shakovxomka@gmail.com>
2025-08-03 15:07:06 +0500

    feat: error dark theme

--- rs-react-app/src/components/errorPage/errorPage.tsx
+++ rs-react-app/src/components/errorPage/errorPage.tsx
@@ -1,15 +1,24 @@
 import { Link } from 'react-router';
 import './errorPage.css';
+import { themeContext } from '../../util/context';
+import { useContext } from 'react';
 
 export default function ErrorPage() {
+  const { theme } = useContext(themeContext);
   return (
-    <div className="error-page__container">
-      <div className="error-page__content">
-        <h1 className="error-page__title">Oops! Something went wrong</h1>
-        <p className="error-page__message">
-          The page youre looking for encountered an error.
+    <div className={`error-page__container ${theme}`}>
+      <div className={`error-page__content ${theme}`}>
+        <h1 className={`error-page__title ${theme}`}>
+          Oops! Something went wrong
+        </h1>
+        <p className={`error-page__message ${theme}`}>
+          The page you&apos;re looking for encountered an error.
         </p>
-        <Link to="/" className="error-page__link" data-testid="back-home-link">
+        <Link
+          to="/"
+          className={`error-page__link ${theme}`}
+          data-testid="back-home-link"
+        >
           Back to Home
         </Link>
       </div>
