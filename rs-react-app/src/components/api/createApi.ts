013ba5417886a57d6ec36ede319781803bb0c2ca
Xomkalol <shakovxomka@gmail.com>
2025-08-08 23:22:28 +0500

    feat: add redux query createApi

---
+++ rs-react-app/src/components/api/createApi.ts
@@ -0,0 +1,42 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+interface PokemonAbility {
+  ability: {
+    name: string;
+    url: string;
+  };
+  is_hidden: boolean;
+  slot: number;
+}
+
+interface PokemonForm {
+  name: string;
+  url: string;
+}
+
+interface Pokemon {
+  name: string;
+  abilities: PokemonAbility[];
+  forms: PokemonForm[];
+  species: {
+    name: string;
+    url: string;
+  };
+  sprites: {
+    front_default: string;
+  };
+}
+// Define a service using a base URL and expected endpoints
+export const pokemonApi = createApi({
+  reducerPath: 'pokemonApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
+  endpoints: (builder) => ({
+    getPokemonByName: builder.query<Pokemon, string>({
+      query: (name) => `pokemon/${name}`,
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetPokemonByNameQuery } = pokemonApi;
