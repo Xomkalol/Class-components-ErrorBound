c9a2c476dd5b620f02be37cb7169c06e81dbabf2
Xomkalol <shakovxomka@gmail.com>
2025-08-15 11:45:10 +0500

    feat: added reducer for a RTK query for a multiply fetch and add to createCSV component

--- rs-react-app/src/components/api/createApi.ts
+++ rs-react-app/src/components/api/createApi.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { getPokemonData } from '../../util/createCSV';
 interface PokemonAbility {
   ability: {
     name: string;
@@ -7,7 +8,6 @@ interface PokemonAbility {
   is_hidden: boolean;
   slot: number;
 }
-
 interface PokemonForm {
   name: string;
   url: string;
@@ -45,7 +45,21 @@ export const pokemonApi = createApi({
       query: (number) => `pokemon/?limit=20&offset=${number}`,
       keepUnusedDataFor: 100000,
     }),
+    getListOfthings: builder.query({
+      queryFn: (ids) => {
+        const promises = ids.map((id: string) => {
+          return getPokemonData(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        });
+        return Promise.all(promises).then((results) => {
+          return { data: results };
+        });
+      },
+    }),
   }),
 });
 
-export const { useGetPokemonByNameQuery, useGetPokemonListQuery } = pokemonApi;
+export const {
+  useGetPokemonByNameQuery,
+  useGetPokemonListQuery,
+  useGetListOfthingsQuery,
+} = pokemonApi;
--- rs-react-app/src/components/flyout/flyout.tsx
+++ rs-react-app/src/components/flyout/flyout.tsx
@@ -5,11 +5,43 @@ import CreateCSVFile from '../../util/createCSV';
 import './flyout.css';
 import { themeContext } from '../../util/context';
 import { useContext } from 'react';
+import { useGetListOfthingsQuery } from '../api/createApi';
+
+interface PokemonAbility {
+  ability: {
+    name: string;
+    url: string;
+  };
+  is_hidden: boolean;
+  slot: number;
+}
+interface PokemonForm {
+  name: string;
+  url: string;
+}
+
+export interface Pokemon {
+  name: string;
+  abilities: PokemonAbility[];
+  forms: PokemonForm[];
+  species: {
+    name: string;
+    url: string;
+  };
+  sprites: {
+    front_default: string;
+  };
+}
 
 export default function FlyOut() {
   const pokeMonState = useSelector((state: RootState) => state.counter);
   const dispatch = useDispatch();
   const { theme } = useContext(themeContext);
+  const pokemonsNames: string[] = [];
+  pokeMonState.pokemons.map((pokemon) => {
+    pokemonsNames.push(pokemon.name);
+  });
+  const { data } = useGetListOfthingsQuery(pokemonsNames);
 
   if (pokeMonState.pokemons.length == 0) {
     return null;
@@ -32,7 +64,7 @@ export default function FlyOut() {
         </button>
         <button
           className={`selected__download-button ${theme}`}
-          onClick={() => CreateCSVFile(pokeMonState)}
+          onClick={() => CreateCSVFile(data)}
           data-testid="download"
         >
           Download
