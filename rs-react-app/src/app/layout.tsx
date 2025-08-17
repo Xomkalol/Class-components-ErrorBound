eefa5cc96d5abdeaafe9c17c1f0a3d4c2ebc6c36
Xomkalol <shakovxomka@gmail.com>
2025-08-17 14:11:08 +0500

    feat: add page/layout

---
+++ rs-react-app/src/app/layout.tsx
@@ -0,0 +1,20 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <link rel="icon" type="image/svg+xml" href="./pokemon-icon.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Vite + React + TS</title>
+      </head>
+      <body>
+        <div id="root"></div>
+        <div id="root">{children}</div>
+      </body>
+    </html>
+  );
+}
