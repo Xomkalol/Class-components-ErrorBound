372b7bf414cc76bacf4981d72b1642a76222705f
Xomkalol <shakovxomka@gmail.com>
2025-08-30 01:05:59 +0500

    feat: added fetch command

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,7 +1,45 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return <></>;
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | unknown>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://nyc3.digitaloceanspaces.com/owid-public/data/co2/owid-co2-data.json'
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response}`);
+        }
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {'error'}</div>;
+  }
+
+  return (
+    <div>
+      {' '}
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
 
 export default App;
